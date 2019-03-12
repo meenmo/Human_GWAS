0a3ae4d37d86cb016ada32a83a55d4bf7e172302
Meenmo <meenmo@outlook.com>
2019-03-12 13:54:32 -0500

    Update cross_ref.sql

--- cross_ref.sql
+++ cross_ref.sql
@@ -4,47 +4,48 @@ USE Human_GWAS
 GO
 CREATE PROCEDURE cross_ref @gene varchar(20), @margin numeric
 	AS 
-	SELECT hg19.chr_start, hg19.chr_end, hg19.gene_name,
-		   [dbo].[BMI_giant_bmi].*,
-		   [dbo].[BMI_japanese_bmi].*,
-		   [dbo].[BMI_ukbb_bmi_Neale].*,
-		   [dbo].[Lipid_Exome_Lu_East_Asian_NG].*,
-		   [dbo].[Lipid_Exome_Lu_European_and_East_Asian_NG].*,
-		   [dbo].[Lipid_GLGC_Exome_Liu_NG].*,
-		   [dbo].[Lipid_GLGC_Willer_NG].*,
-		   [dbo].[Lipid_Japanese_lipid_trait_Kanai_NG].*,
-		   [dbo].[Lipid_MVP_Klarin_NG].*,
-		   [dbo].[Lipid_prins_SCientific_REPORT].*,
-		   [dbo].[Lipid_UKBB_lipid_trait_Neale].*,
-		   [dbo].[Lipid_UKBB_statin_usage_Neale].*
+	SELECT a.chr_start, a.chr_end, a.gene_name,
+	       b.*,
+	       c.*,
+	       d.*,
+	       e.*,
+	       f.*,
+	       g.*,
+	       h.*,
+	       i.*,
+	       j.*,
+	       k.*,
+	       l.*,
+	       m.*
+		   
 
-	FROM   [dbo].[hg19],
-		   [dbo].[BMI_giant_bmi],
-		   [dbo].[BMI_japanese_bmi],
-		   [dbo].[BMI_ukbb_bmi_Neale],
-		   [dbo].[Lipid_Exome_Lu_East_Asian_NG],
-		   [dbo].[Lipid_Exome_Lu_European_and_East_Asian_NG],
-		   [dbo].[Lipid_GLGC_Exome_Liu_NG],
-		   [dbo].[Lipid_GLGC_Willer_NG],
-		   [dbo].[Lipid_Japanese_lipid_trait_Kanai_NG],
-		   [dbo].[Lipid_MVP_Klarin_NG],
-		   [dbo].[Lipid_prins_SCientific_REPORT],
-		   [dbo].[Lipid_UKBB_lipid_trait_Neale],
-		   [dbo].[Lipid_UKBB_statin_usage_Neale]
+	FROM   [dbo].[hg19] as a,
+		   [dbo].[BMI_giant_bmi] as b,
+		   [dbo].[BMI_japanese_bmi] as c,
+		   [dbo].[BMI_ukbb_bmi_Neale] as d,
+		   [dbo].[Lipid_Exome_Lu_East_Asian_NG] as e,
+		   [dbo].[Lipid_Exome_Lu_European_and_East_Asian_NG] as f,
+		   [dbo].[Lipid_GLGC_Exome_Liu_NG] as g,
+		   [dbo].[Lipid_GLGC_Willer_NG] as h,
+		   [dbo].[Lipid_Japanese_lipid_trait_Kanai_NG] as i,
+		   [dbo].[Lipid_MVP_Klarin_NG] as j,
+		   [dbo].[Lipid_prins_SCientific_REPORT] as k,
+		   [dbo].[Lipid_UKBB_lipid_trait_Neale] as l,
+		   [dbo].[Lipid_UKBB_statin_usage_Neale] as m
  
 	WHERE  gene_name = @gene and
-		   [dbo].[BMI_giant_bmi].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and
-		   [dbo].[BMI_japanese_bmi].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and
-		   [dbo].[BMI_ukbb_bmi_Neale].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and
-		   [dbo].[Lipid_Exome_Lu_East_Asian_NG].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and
-		   [dbo].[Lipid_Exome_Lu_European_and_East_Asian_NG].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and
-		   [dbo].[Lipid_GLGC_Exome_Liu_NG].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and	
-		   [dbo].[Lipid_GLGC_Willer_NG].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and
-		   [dbo].[Lipid_Japanese_lipid_trait_Kanai_NG].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and
-		   [dbo].[Lipid_MVP_Klarin_NG].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and
-		   [dbo].[Lipid_prins_SCientific_REPORT].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and
-		   [dbo].[Lipid_UKBB_lipid_trait_Neale].bp between hg19.chr_start-@margin and hg19.chr_end+@margin and
-		   [dbo].[Lipid_UKBB_statin_usage_Neale].bp between hg19.chr_start-@margin and hg19.chr_end+@margin
+		   b.bp between a.chr_start-@margin and a.chr_end+@margin or
+		   c.bp between a.chr_start-@margin and a.chr_end+@margin or
+		   d.bp between a.chr_start-@margin and a.chr_end+@margin or
+		   e.bp between a.chr_start-@margin and a.chr_end+@margin or
+		   f.bp between a.chr_start-@margin and a.chr_end+@margin or
+		   g.bp between a.chr_start-@margin and a.chr_end+@margin or	
+		   h.bp between a.chr_start-@margin and a.chr_end+@margin or
+		   i.bp between a.chr_start-@margin and a.chr_end+@margin or
+		   j.bp between a.chr_start-@margin and a.chr_end+@margin or
+		   k.bp between a.chr_start-@margin and a.chr_end+@margin or
+		   l.bp between a.chr_start-@margin and a.chr_end+@margin or
+		   m.bp between a.chr_start-@margin and a.chr_end+@margin
 
 GO
 
