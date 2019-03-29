2c05cc298cde37b53b3bf207602b2d24e297cf69
Meenmo <meenmo@outlook.com>
2019-03-29 15:02:36 -0500

    Update load.py

--- load.py
+++ load.py
@@ -5,89 +5,113 @@ while True:
     #Connect to SQL Server
     sql_conn =  pyodbc.connect('DRIVER={ODBC Driver 11 for SQL Server};SERVER=PARKSLAB;DATABASE=Human_GWAS;Trusted_Connection=Yes')
 
-    choose_table = input("Which tables do you want to include?\n\n 1. BMI_giant_bmi\n 2. BMI_japanese_bmi \n 3. BMI_ukbb_bmi_Neale \n 4. Lipid_Engage_Surakka_NG \n 5. Lipid_Exome_Lu_East_Asian_NG \n 6. Lipid_Exome_Lu_European_and_East_Asian_NG \n 7. Lipid_GLGC_Willer_NG \n 8. Lipid_Japanese_lipid_trait_Kanai_NG \n 9. Lipid_MVP_Klarin_NG \n 10.Lipid_Spracklen_Hum_Mol_Genetics \n 11.Lipid_UKBB_high_cholesterol_ukbb_Connor_alkesgroup \n 12.Lipid_UKBB_lipid_trait_Neale \n 13.Lipid_UKBB_statin_usage_Neale\n\nChoose the options seperataed by comma. e.g.) 1,3,5\n")
+    choose_table = input("Which tables do you want to include?\n\n 1. BMI_giant_bmi\n 2. BMI_japanese_bmi \n 3. BMI_ukbb_bmi_Neale \n 4. Lipid_Engage_Surakka_NG \n 5. Lipid_Exome_Lu_East_Asian_NG \n 6. Lipid_Exome_Lu_European_and_East_Asian_NG \n 7. Lipid_GLGC_Willer_NG \n 8. Lipid_Japanese_lipid_trait_Kanai_NG \n 9. Lipid_MVP_Klarin_NG \n 10.Lipid_Spracklen_Hum_Mol_Genetics \n 11.Lipid_UKBB_high_cholesterol_ukbb_Connor_alkesgroup \n 12.Lipid_UKBB_lipid_trait_Neale \n 13.Lipid_UKBB_statin_usage_Neale\n\nEnter the table numbers that you want to include seperataed by comma.\ne.g.) 1,3,5\nIf you want to choose all tables, then enter *.\n")
 
     #Obtain index of chosen tables as a list
-    chosen_list = [int(i) for i in choose_table.split(",")]
+    if choose_table == '*':
+        chosen_list = [i for i in range(1,14)]
+    else:
+        chosen_list = [int(i) for i in choose_table.split(",")]
 
+    table_list = []
+    table_name = []
     for i in chosen_list:
-        ct = 1
-        if ct == i:
-            #Load Tables from SQL Server into Pandas DataFrame
-            hg19 = pd.read_sql("SELECT * FROM hg19", sql_conn)
-            continue
-        ct += 1
+        count = 1
 
-        if ct == i:
+        if count == i:
             giant = pd.read_sql("SELECT TOP 500 * FROM BMI_giant_bmi", sql_conn)
+            table_list.append(giant)
+            table_name.append('giant')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             japanese = pd.read_sql("SELECT TOP 500 * FROM BMI_japanese_bmi", sql_conn)
+            table_list.append(japanese)
+            table_name.append('japanese')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             ukbb_bmi = pd.read_sql("SELECT TOP 500 * FROM BMI_ukbb_bmi_Neale", sql_conn)
+            table_list.append(ukbb_bmi)
+            table_name.append('ukbb_bmi')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             surakka = pd.read_sql("SELECT TOP 500 * FROM Lipid_Engage_Surakka_NG", sql_conn)
+            table_list.append(surakka)
+            table_name.append('surakka')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             east_asian = pd.read_sql("SELECT TOP 500 * FROM Lipid_Exome_Lu_East_Asian_NG", sql_conn)
+            table_list.append(east_asian)
+            table_name.append('east_asian')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             european = pd.read_sql("SELECT TOP 500 * FROM Lipid_Exome_Lu_European_and_East_Asian_NG", sql_conn)
+            table_list.append(european)
+            table_name.append('european')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             glgc = pd.read_sql("SELECT TOP 500 * FROM Lipid_GLGC_Willer_NG", sql_conn)
+            table_list.append(glgc)
+            table_name.append('glgc')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             lipid_japanese = pd.read_sql("SELECT TOP 500 * FROM Lipid_Japanese_lipid_trait_Kanai_NG", sql_conn)
+            table_list.append(lipid_japanese)
+            table_name.append('lipid_japanese')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             lipid_mvp = pd.read_sql("SELECT TOP 500 * FROM Lipid_MVP_Klarin_NG", sql_conn)
+            table_list.append(lipid_mvp)
+            table_name.append('lipid_mvp')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             lipid_spracklen = pd.read_sql("SELECT TOP 500 * FROM Lipid_Spracklen_Hum_Mol_Genetics", sql_conn)
+            table_list.append(lipid_spracklen)
+            table_name.append('lipid_spracklen')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             high_chol = pd.read_sql("SELECT TOP 500 * FROM Lipid_UKBB_high_cholesterol_ukbb_Connor_alkesgroup", sql_conn)
+            table_list.append(high_chol)
+            table_name.append('high_chol')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             ukbb_lipid = pd.read_sql("SELECT TOP 500 * FROM Lipid_UKBB_lipid_trait_Neale", sql_conn)
+            table_list.append(ukbb_lipid)
+            table_name.append('ukbb_lipid')
             continue
-        ct += 1
+        count += 1
 
-        if ct == i:
+        if count == i:
             ukbb_statin = pd.read_sql("SELECT TOP 500 * FROM Lipid_UKBB_statin_usage_Neale", sql_conn)
-            continue
+            table_list.append(ukbb_statin)
+            table_name.append('ukbb_statin')
 
+
+    hg19 = pd.read_sql("SELECT * FROM hg19", sql_conn)
     #Makes all gene names into lower cases since SQL is not case-sensitive, but Python is.
     hg19['gene_name'] = hg19['gene_name'].str.lower()
     hg19['table_name'] = 'aw011738'
-
-    table_list = [giant,japanese,ukbb_bmi,surakka,east_asian,european,glgc,lipid_japanese,lipid_mvp,lipid_spracklen,high_chol,ukbb_lipid,ukbb_statin]
-    table_name = ["giant","japanese","ukbb_bmi","surakka","east_asian","european","glgc","lipid_japanese","lipid_mvp","lipid_spracklen","high_chol","ukbb_lipid","ukbb_statin"]
 
     gene_name = input("Type gene name: ").lower()
     start = min(hg19.loc[(hg19['gene_name']==gene_name)]['chr_start'])
@@ -126,5 +150,6 @@ while True:
     df['bp'] = df['bp'].astype(int)
     #Drop irrelevant rows: chr which does not fall into the selected gene name
     df = df.loc[df['chr'].isin(chr)]
+    #print(df)
+    #print('')
     print(df)
-    print('')
