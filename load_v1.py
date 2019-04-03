0ff2705c55835990a567c48e6308efe9ad9b76e0
Meenmo <meenmo@outlook.com>
2019-04-03 16:31:58 -0500

    Update load_v1.py

--- load_v1.py
+++ load_v1.py
@@ -8,13 +8,14 @@ while True:
     while True:
 
         #prompt to get input which table to include
-        choose_table = input("Which tables do you want to include?\n\n 1. BMI_giant_bmi\n 2. BMI_japanese_bmi \n 3. BMI_ukbb_bmi_Neale \n 4. Lipid_Engage_Surakka_NG \n 5. Lipid_Exome_Lu_East_Asian_NG \n 6. Lipid_Exome_Lu_European_and_East_Asian_NG \n 7. Lipid_GLGC_Willer_NG \n 8. Lipid_Japanese_lipid_trait_Kanai_NG \n 9. Lipid_MVP_Klarin_NG \n 10.Lipid_Spracklen_Hum_Mol_Genetics \n 11.Lipid_UKBB_high_cholesterol_ukbb_Connor_alkesgroup \n 12.Lipid_UKBB_lipid_trait_Neale \n 13.Lipid_UKBB_statin_usage_Neale\n\nEnter the table numbers that you want to include seperataed by comma.\ne.g.) 1,3,5\nIf you want to choose all tables, then enter *.\n")
+        choose_table = input("Which tables do you want to include?\n\n 1. BMI_giant_bmi\n 2. BMI_japanese_bmi \n 3. BMI_ukbb_bmi_Neale \n  4. Lipid_Engage_Surakka_NG \n 5. Lipid_Exome_Lu_East_Asian_NG \n 6. Lipid_Exome_Lu_European_and_East_Asian_NG \n 7. Lipid_GLGC_Willer_NG \n 8. Lipid_Japanese_lipid_trait_Kanai_NG \n 9. Lipid_MVP_Klarin_NG \n 10.Lipid_Spracklen_Hum_Mol_Genetics \n 11.Lipid_UKBB_high_cholesterol_ukbb_Connor_alkesgroup \n 12.Lipid_UKBB_lipid_trait_Neale \n 13.Lipid_UKBB_statin_usage_Neale\n\nEnter the table numbers that you want to include seperataed by comma.\ne.g.) 1,3,5\nIf you want to choose all tables, then enter *.\n")
 
         #Obtain index of chosen tables as a list
         try:
             #select all
             if choose_table == '*':
                 chosen_list = [i for i in range(1,14)]
+                print(type(chosen_list))
                 break
 
             #select multiple tables
@@ -25,9 +26,11 @@ while True:
 
             #select single table
             elif int(choose_table) in [i for i in range(1,14)]:
-                chosen_list = int(choose_table)
-                break
+                chosen_list = [int(choose_table)]
 
+
+                break
+            
 
         except ValueError:
             print('Please enter valid input!')
@@ -145,18 +148,18 @@ while True:
     df = pd.DataFrame(columns=df_col)
 
     count=0
-    for table in table_list:
+    for table in table_list: 
         try:
             #Add a column named 'table_name' whose values are corresponding table name
             table['table_name'] = table_name[count]
 
             #each column that satisfies the conditions.
-            a = table.loc[(start <= table['bp']) & (table['bp'] <= end)]['bp'].to_frame()
-            b = table.loc[(start <= table['bp']) & (table['bp'] <= end)]['chr'].to_frame()
-            c = table.loc[(start <= table['bp']) & (table['bp'] <= end)]['beta'].to_frame()
-            d = table.loc[(start <= table['bp']) & (table['bp'] <= end)]['p_value'].to_frame()
-            e = table.loc[(start <= table['bp']) & (table['bp'] <= end)]['trait'].to_frame()
-            f = table.loc[(start <= table['bp']) & (table['bp'] <= end)]['table_name'].to_frame()
+            a = table.loc[(start <= table['bp']) & (table['bp'] <= end) & (table['chr'].isin(chr))]['bp'].to_frame()
+            b = table.loc[(start <= table['bp']) & (table['bp'] <= end) & (table['chr'].isin(chr))]['chr'].to_frame()
+            c = table.loc[(start <= table['bp']) & (table['bp'] <= end) & (table['chr'].isin(chr))]['beta'].to_frame()
+            d = table.loc[(start <= table['bp']) & (table['bp'] <= end) & (table['chr'].isin(chr))]['p_value'].to_frame()
+            e = table.loc[(start <= table['bp']) & (table['bp'] <= end) & (table['chr'].isin(chr))]['trait'].to_frame()
+            f = table.loc[(start <= table['bp']) & (table['bp'] <= end) & (table['chr'].isin(chr))]['table_name'].to_frame()
 
             #merge these all together
             df_temp = a.join(b).join(c).join(d).join(e).join(f)
@@ -164,13 +167,13 @@ while True:
             df = df.append(df_temp)
             count+=1
 
-        except TypeError:
+            except TypeError:
             continue
 
     #Convert 'bp' into integer
     df['bp'] = df['bp'].astype(int)
     #Drop irrelevant rows: chr which does not fall into the selected gene name
-    df = df.loc[df['chr'].isin(chr)]
+    #df = df.loc[df['chr'].isin(chr)]
     #print(df)
     #print('')
     print(df)
